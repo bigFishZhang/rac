d7a6399aae4ba7cc004c9a48e4d25640b0b1d95a
zzbbigfish <zzbbigfish@gmail.com>
2019-06-14 17:07:29 +0800

    rac signal

---
+++ RacTest/RacTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RacTest
+//
+//  Created by bigfish on 2019/6/14.
+//  Copyright © 2019 zzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RacTest/RacTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RacTest
+//
+//  Created by bigfish on 2019/6/14.
+//  Copyright © 2019 zzb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
